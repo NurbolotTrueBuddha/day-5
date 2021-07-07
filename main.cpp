700c4e4411810f72e679287f2ef08979be50ed60
NurbolotTrueBuddha <86740145+NurbolotTrueBuddha@users.noreply.github.com>
2021-07-07 12:17:25 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    
+    int i = 0;
+
+    while (window.isOpen())
+    {
+        sf::CircleShape shape(50.f,3);
+        shape.setPosition(100.f, 100.f);
+        shape.setOrigin(50.f,50.f);
+        shape.setFillColor(sf::Color::Red);
+        shape.setRotation(1.f+i);
+        sf::Event event;
+        // че палиш
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.draw(shape);
+       
+        window.display();
+        
+        if (event.type == sf::Event::EventType::KeyPressed){
+        
+        i+=1;
+
+        if (i == 360) i = 0;
+        }}
+
+    return 0;
+}
